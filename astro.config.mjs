ad582b52ded0e3ba11c18bf35c2a1e4652e76a27
Garrett Koller <gkoller@catalist.us>
2025-09-15 10:36:53 +0200

    Moved all contact information to `me.ts` script
    and added instructions to README to check there

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 import compress from 'astro-compress';
-
 import preact from '@astrojs/preact';
 
 export default defineConfig({
